a2558de9329e020332c4f7055a344ff40e5142ca
liushuitanliunian <229449379@qq.com>
2021-12-06 09:43:59 +0800

    Update sendMail.js

--- sendMail.js
+++ sendMail.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 
 const sendMail = async (data) => {
   let transporter = nodemailer.createTransport({
-    host: 'smtp.qq.com',
+    host: 'smtp.163.com',
     port: '465',
     secureConnection: true,
     auth: {
