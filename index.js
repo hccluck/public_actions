cfd968798aa3690ffc46832b2ac539318fcda086
俗 <45392175+yu945882678@users.noreply.github.com>
2022-01-17 14:36:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,63 +2,65 @@ const fetch = require('node-fetch');
 const sign_in = require('./src/signIn');
 const draw = require('./src/draw');
 const dipLucky = require('./src/dipLucky');
-const sendMail = require('./src/sendMail');
+const { headers, webhook, phone } = require('./src/config');
+const axios = require('axios')
 
-const { headers } = require('./src/config');
+  (async () => {
+    let sign_res = '';
 
-(async () => {
-  let sign_res = '';
+    try {
+      sign_res = await sign_in();
+    } catch (error) {
+      sign_res = error;
+    }
 
-  try {
-    sign_res = await sign_in();
-  } catch (error) {
-    sign_res = error;
-  }
+    console.log(sign_res);
 
-  console.log(sign_res);
+    let draw_res = '';
+    try {
+      draw_res = await draw();
+    } catch (error) {
+      draw_res = error;
+    }
 
-  let draw_res = '';
-  try {
-    draw_res = await draw();
-  } catch (error) {
-    draw_res = error;
-  }
+    console.log(draw_res);
 
-  console.log(draw_res);
+    // 当前分数
+    const { data } = await fetch('https://api.juejin.cn/growth_api/v1/get_cur_point', {
+      headers,
+      method: 'GET',
+      credentials: 'include'
+    }).then((res) => res.json());
 
-  // 当前分数
-  const { data } = await fetch('https://api.juejin.cn/growth_api/v1/get_cur_point', {
-    headers,
-    method: 'GET',
-    credentials: 'include'
-  }).then((res) => res.json());
+    console.log(`当前矿石：${data}`);
 
-  console.log(`当前矿石：${data}`);
+    let dip_res;
+    try {
+      dip_res = await dipLucky();
+    } catch (error) {
+      dip_res = error;
+    }
 
-  let dip_res;
-  try {
-    dip_res = await dipLucky();
-  } catch (error) {
-    dip_res = error;
-  }
+    console.log(dip_res);
 
-  console.log(dip_res);
+    try {
+      await axios.post(webhook, {
+        msgtype: "markdown",
+        markdown: {
+          title: "掘金",
+          text: `### 掘金任务结果 @${phone} \n > #### 签到结果：${sign_res} \n > #### 抽奖结果：${draw_res} \n > #### 沾喜气结果：${dip_res} \n > #### 当前矿石：${data} \n`
+        },
+        at: {
+          atMobiles: [
+            "'" + phone + "'"
+          ]
+        }
+      }).then(res => {
+        console.log(res)
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  })();
 
-  try {
-    await sendMail({
-      from: '掘金',
-      subject: '定时任务',
-      html: `
-        <h1 style="text-align: center">自动签到通知</h1>
-        <p style="text-indent: 2em">签到结果：${sign_res}</p>
-        <p style="text-indent: 2em">抽奖结果：${draw_res}</p>
-        <p style="text-indent: 2em">沾喜气结果：${dip_res}</p>
-        <p style="text-indent: 2em">当前矿石：${data}</p><br/>
-      `
-    });
 
-    console.log('邮件发送成功！');
-  } catch (error) {
-    console.error(error);
-  }
-})();
