56eeff89571ad194c90fbfe74d4f6cc44802c83c
俗 <45392175+yu945882678@users.noreply.github.com>
2022-01-21 16:08:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ const axios = require('axios');
     try {
       await axios.post(gettokenURL).then(res => {
         access_token = res.access_token
+        console.log('token:',access_token)
       })
       
     } catch (error){
