7a594def24759e47906d3423cc23c28632702eb4
Cain <729048330@qq.com>
2022-02-01 14:38:26 +0800

    Update sendMail.js

--- src/sendMail.js
+++ src/sendMail.js
@@ -19,7 +19,7 @@ const sendMail = async (data) => {
   if (!EmailReg.test(user)) return Promise.reject('未正确配置邮箱账号格式！！！');
 
   try {
-    const [, type] = smtp_host || user.split('@');
+    const [, type] = smtp_host ? smtp_host : user.split('@');
 
     transporter = nodemailer.createTransport({
       host: `smtp.${type || 'qq.com'}`,
