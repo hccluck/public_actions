f60ca1f4804de8480c0c9bd455b59428449b8950
hccluck <hccluck@126.com>
2022-02-09 15:49:35 +0800

    优化

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const [cookie, user, pass, to, uid] = process.argv.slice(2);
+const { COOKIE, USER, PASS, TO, UID } = process.env;
 
 const headers = {
   'content-type': 'application/json; charset=utf-8',
@@ -9,7 +9,7 @@ const headers = {
   'sec-ch-ua-mobile': '?0',
   referer: 'https://juejin.cn/',
   accept: '*/*',
-  cookie
+  cookie: COOKIE
 };
 
-module.exports = { headers, user, pass, to, uid };
+module.exports = { headers, user: USER, pass: PASS, to: TO, uid: UID };
