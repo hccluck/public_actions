e4c61229aa088711ed124671aeb77d407feb7216
俗 <45392175+yu945882678@users.noreply.github.com>
2022-02-09 13:13:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const sign_in = require('./src/signIn');
 const draw = require('./src/draw');
 const dipLucky = require('./src/dipLucky');
 const { headers, webhook, phone, corpid,corpsecret, touser, agentid } = require('./src/config');
-console.log('环境值1')
 const axios = require('axios');
   (async ()=>{
     let sign_res = '';
